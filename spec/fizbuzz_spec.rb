c16d655d88a571e00336b20158783e710a3ee55f
Tolvic <terence.h.mace@gmail.com>
2018-09-19 15:10:12 +0100

    Code now passes multiples of 3 = fizz requirement. New unit test add to check for when 10 is passed

--- spec/fizbuzz_spec.rb
+++ spec/fizbuzz_spec.rb
@@ -6,6 +6,12 @@ describe 'fizzbuzz' do
 end
 
 describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
