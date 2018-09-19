6e3d2485726452d0f2ccc9a6c3b3b800c8eb41e6
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-09-19 16:37:09 +0100

    refactored tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,4 +97,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.color = true
+  config.formatter = :documentation # :progress, :html, :textmate
 end
